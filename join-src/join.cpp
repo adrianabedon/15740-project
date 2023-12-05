e7fbbab130dc353cc3ff00ad507d04e6671a3cdd
adrianabedon <abedon.adrian@gmail.com>
2023-12-05 00:06:31 -0500

    implement build

---
+++ join-src/join.cpp
@@ -0,0 +1,179 @@
+#include <cassert>
+#include "join.h"
+
+CHashJoin::CHashJoin()
+{
+    for (int i = 0; i < NUM_BUCKETS; i++)
+    {
+        for (int j = 0; j < NUM_SLOTS; j++)
+        {
+            buckets[i].slots[j].status = 0;
+            buckets[i].slots[j].tag = 0;
+            buckets[i].slots[j].head = 0;
+        }
+        buckets[i].collision_slot = i % NUM_SLOTS;
+    }
+
+    for (int i = 0; i < NUM_SLOTS; i++)
+    {
+        for (int j = 0; j < NUM_ADDRESS_TABLES_SLOT; j++)
+        {
+            address_tables[i].entries[j].rid = 0;
+            address_tables[i].entries[j].key = 0;
+            address_tables[i].entries[j].next = 0;
+        }
+        address_tables[i].size = 0;
+    }
+}
+
+hash_t hash1(Key_t key)
+{
+    unsigned int x = static_cast<unsigned int>(key);
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = (x >> 16) ^ x;
+    hash_t y = static_cast<hash_t>(x & ((1 << HASH_BITS) - 1));
+    return y;
+}
+
+hash_t hash2(Key_t key)
+{
+    unsigned int x = static_cast<unsigned int>(key);
+    hash_t y = static_cast<hash_t>(x & ((1 << HASH_BITS) - 1));
+    return y;
+}
+
+void CHashJoin::Join(std::vector<input_tuple_t> &relR, std::vector<input_tuple_t> &relS, std::vector<output_tuple_t> &relRS)
+{
+    Build(relR);
+    Probe(relS, relRS);
+}
+
+bool CHashJoin::insert_tuple_to_empty_slot(hash_t hash, atindex_t slot_idx, RID_t rid, Key_t key, hash_t tag)
+{
+    bucket_t &bucket = buckets[hash];
+    slot_t &slot = bucket.slots[slot_idx];
+
+    if (slot.status != 0)
+    {
+        return false;
+    }
+
+    address_table_t &address_table = address_tables[slot_idx];
+    atindex_t next = address_table.size;
+
+    slot.status = 1;
+    slot.tag = tag;
+    slot.head = next;
+
+    address_table.entries[next].rid = rid;
+    address_table.entries[next].key = key;
+    address_table.entries[next].next = 0;
+    address_table.size++;
+
+    return true;
+}
+
+bool CHashJoin::eject_slot(hash_t hash, slotidx_t slot_idx)
+{
+    bucket_t &bucket = buckets[hash];
+    slot_t &slot = bucket.slots[slot_idx];
+
+    assert(slot.status != 0); // slot must be occupied
+
+    bucket_t &bucket_alt = buckets[slot.tag];
+    for (int i = 0; i < NUM_SLOTS; i++)
+    {
+        slot_t &slot_alt = bucket_alt.slots[i];
+        if (slot_alt.status == 0)
+        {
+            slot_alt.status = 1;
+            slot_alt.tag = slot.tag;
+            slot_alt.head = slot.head;
+            slot.status = 0;
+            slot.tag = 0;
+            slot.head = 0;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void CHashJoin::insert_tuple_to_collision_slot(hash_t hash, RID_t rid, Key_t key)
+{
+    bucket_t &bucket = buckets[hash];
+    slot_t &slot = bucket.slots[bucket.collision_slot];
+
+    assert(slot.status != 0); // slot must be occupied
+
+    address_table_t &address_table = address_tables[bucket.collision_slot];
+    atindex_t next = address_table.size;
+
+    address_table.entries[next].rid = rid;
+    address_table.entries[next].key = key;
+    address_table.entries[next].next = slot.head;
+    address_table.size++;
+
+    // insert as tail of collision chain (why tho????)
+    atindex_t curr = slot.head;
+    while (address_table.entries[curr].next != 0)
+    {
+        curr = address_table.entries[curr].next;
+    }
+
+    address_table.entries[curr].next = next;
+
+    bucket.collision_slot = (bucket.collision_slot + 1) % NUM_SLOTS;
+    return;
+}
+
+void CHashJoin::Build(std::vector<input_tuple_t> &relR)
+{
+    for (input_tuple_t &tuple : relR)
+    {
+        // first try to insert into an empty slot
+        hash_t hash1_val = hash1(tuple.key);
+        hash_t hash2_val = hash2(tuple.key);
+        bool inserted = false;
+        for (int slot_idx = 0; slot_idx < NUM_SLOTS; slot_idx++)
+        {
+            if (insert_tuple_to_empty_slot(hash1_val, slot_idx, tuple.rid, tuple.key, hash2_val))
+            {
+                inserted = true;
+                break;
+            }
+            if (insert_tuple_to_empty_slot(hash2_val, slot_idx, tuple.rid, tuple.key, hash1_val))
+            {
+                inserted = true;
+                break;
+            }
+        }
+
+        if (inserted)
+            continue;
+
+        // next try to eject a slot
+        for (int slot_idx = 0; slot_idx < NUM_SLOTS; slot_idx++)
+        {
+            if (eject_slot(hash1_val, slot_idx))
+            {
+                inserted = insert_tuple_to_empty_slot(hash1_val, slot_idx, tuple.rid, tuple.key, hash2_val);
+                assert(inserted);
+                break;
+            }
+            if (eject_slot(hash2_val, slot_idx))
+            {
+                inserted = insert_tuple_to_empty_slot(hash2_val, slot_idx, tuple.rid, tuple.key, hash1_val);
+                assert(inserted);
+                break;
+            }
+        }
+
+        if (inserted)
+            continue;
+
+        // next add to chain at hash1 bucket collision slot
+        insert_tuple_to_collision_slot(hash1_val, tuple.rid, tuple.key);
+    }
+}
---
+++ join-src/join.h
@@ -0,0 +1,96 @@
+#ifndef __JOIN_H__
+#define __JOIN_H__
+
+#include <vector>
+#include <cstdint>
+
+#define HASH_BITS 12
+#define NUM_SLOTS 4
+#define NUM_BUCKETS (2 >> HASH_BITS) // 2^12 = 4096
+#define NUM_ADDRESS_TABLES_SLOT 5000 // this is the number of slots in each address table
+                                     // max size of inner relation in NUM_SLOT * NUM_ADDRESS_TABLES_SLOT
+
+// they use 12 bit wide hash function
+/** We are implementing N to 1 join, so if the tuple in the second
+ * relation can only map to one tuple in the first relation.
+ */
+
+typedef uint8_t slotidx_t;
+typedef uint16_t hash_t;
+typedef uint16_t atindex_t;
+typedef int RID_t;
+typedef int Key_t;
+typedef struct
+{
+  RID_t rid;
+  Key_t key;
+} input_tuple_t;
+typedef struct
+{
+  RID_t rid1;
+  RID_t rid2;
+  Key_t key;
+} output_tuple_t;
+
+typedef struct
+{
+  // occupied or availale (only need 1 bit)
+  char status;
+  // candidate bucket number of this element
+  // Value of second hash function (12 bits)
+  hash_t tag;
+  // entry point of first tuple in this slot (in the address tables) (log_2_N bits (N is size of first relation))
+  atindex_t head;
+} slot_t;
+
+typedef struct
+{
+  slot_t slots[NUM_SLOTS];
+  // This is the slot to insert into for a collision chain
+  // only need 2 bits
+  // 1. Initiate once in round robin fashion
+  // 2. Increment its value each time it is used (mod 4 but don't need to worry on FPGA)
+  slotidx_t collision_slot;
+} bucket_t;
+
+typedef struct
+{
+  RID_t rid;
+  Key_t key;      // K bits (K = 32 here)
+  atindex_t next; // next tuple
+} address_table_entry_t;
+
+typedef struct
+{
+  address_table_entry_t entries[NUM_ADDRESS_TABLES_SLOT];
+  atindex_t size;
+} address_table_t;
+
+class CHashJoin
+{
+public:
+  CHashJoin();
+  ~CHashJoin();
+  /**
+   * @brief
+   *
+   * @param relR Inner relation
+   * @param relS Outer relation
+   * @param relRS Result relation #TODO FIX THIS TYPE
+   *
+   */
+  void Join(std::vector<input_tuple_t> &relR, std::vector<input_tuple_t> &relS, std::vector<output_tuple_t> &relRS);
+
+private:
+  bool CHashJoin::insert_tuple_to_empty_slot(hash_t hash, atindex_t slot_idx, RID_t rid, Key_t key, hash_t tag);
+  void CHashJoin::insert_tuple_to_collision_slot(hash_t hash, RID_t rid, Key_t key);
+  bool CHashJoin::eject_slot(hash_t hash, slotidx_t slot_idx);
+  void Build(std::vector<input_tuple_t> &relR);
+  void Probe(std::vector<input_tuple_t> &relS, std::vector<output_tuple_t> &relRS);
+
+  bucket_t buckets[NUM_BUCKETS];
+  // NUM_SLOTS address tables
+  address_table_t address_tables[NUM_SLOTS];
+};
+
+#endif // __JOIN_H__
